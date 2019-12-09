e547bed94cc93a2158bc98d20544a29e98a0abab
Vishal Mahajan <vishal423@gmail.com>
2019-12-09 19:38:48 +0530

    fix unit tests

--- projects/ngx-hipster/src/lib/ngx-error/ngx-error.component.spec.ts
+++ projects/ngx-hipster/src/lib/ngx-error/ngx-error.component.spec.ts
@@ -1,25 +1,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonModule } from '@angular/common';
 import { NgxErrorComponent } from './ngx-error.component';
+import {
+  FormGroup,
+  Validators,
+  FormControl,
+  FormBuilder,
+  ReactiveFormsModule
+} from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'hip-test-host',
+  template: `
+    <hip-ngx-error [control]="form.get('test')" label="Test"></hip-ngx-error>
+  `
+})
+class HostStubComponent {
+  form: FormGroup;
+  @ViewChild(NgxErrorComponent, { static: true })
+  public errorComponent: NgxErrorComponent;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({ test: fb.control('', [Validators.required]) });
+  }
+}
 
 describe('NgxErrorComponent', () => {
   let component: NgxErrorComponent;
-  let fixture: ComponentFixture<NgxErrorComponent>;
+  let hostComponent: HostStubComponent;
+  let fixture: ComponentFixture<HostStubComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [NgxErrorComponent],
-      imports: []
+      declarations: [NgxErrorComponent, HostStubComponent],
+      imports: [CommonModule, ReactiveFormsModule]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NgxErrorComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(HostStubComponent);
+    hostComponent = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(hostComponent).toBeTruthy();
+    expect(hostComponent.errorComponent).toBeTruthy();
   });
 });
