cda2705ee64ae6b69d30340668da3cd13af96749
Vishal Mahajan <vishal423@gmail.com>
2019-12-12 17:35:57 +0530

    Fix prettier issues with new project

--- projects/ngx-hipster/schematics/utils/prettier-util.ts
+++ projects/ngx-hipster/schematics/utils/prettier-util.ts
@@ -1,6 +1,7 @@
 import { Tree, Rule } from '@angular-devkit/schematics';
 const prettier = require('prettier');
 import { normalize } from 'path';
+import * as fs from 'fs';
 
 export function applyPrettierOnFile(options: { path: string }): Rule {
   return (tree: Tree) => {
@@ -35,12 +36,21 @@ function prettierApply(tree: Tree, filePath: string) {
     return;
   }
 
-  const prettierOptions =
-    prettier.resolveConfig.sync(filePath, {
+  let prettierOptions: any = {};
+  if (fs.existsSync(normalize('.prettierrc'))) {
+    prettierOptions = prettier.resolveConfig.sync(filePath, {
       editorconfig: true,
       config: normalize('.prettierrc')
-    }) || {};
-  prettierOptions.filepath = filePath;
+    });
+  } else {
+    prettierOptions = prettier.resolveConfig.sync(filePath, {
+      editorconfig: true,
+      config: normalize('node_modules/ngx-hipster/.prettierrc')
+    });
+  }
+
+  prettierOptions.filepath = normalize(filePath);
+
   tree.overwrite(
     filePath,
     prettier.format(buffer.toString('utf-8'), prettierOptions)
