870160c5cc53200d00d1a8b806fe201753291a84
Vishal Mahajan <vishal423@gmail.com>
2019-12-15 01:00:06 +0530

    Publish ngx-hipster demo site

--- ngx-hipster-sample/src/app/movie/movie.ts
+++
@@ -1,6 +0,0 @@
-export interface Movie {
-  id?: number;
-  title: string;
-  plot?: string;
-  genre: string;
-}
--- ngx-hipster-sample/src/jest.conf.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'jest-preset-angular',
-  rootDir: '../',
-  testPathIgnorePatterns: ['<rootDir>/src/test.ts'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupJest.ts']
-};
