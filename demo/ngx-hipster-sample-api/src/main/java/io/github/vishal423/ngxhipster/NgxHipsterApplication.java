b1e3457aefae8d3f8798c91da56decd47f774e61
Vishal Mahajan <vishal423@gmail.com>
2020-02-23 00:13:54 +0530

    Add sample spring boot reactive api project that can be used in the e2e tests / demo

---
+++ demo/ngx-hipster-sample-api/src/main/java/io/github/vishal423/ngxhipster/NgxHipsterApplication.java
@@ -0,0 +1,13 @@
+package io.github.vishal423.ngxhipster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgxHipsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgxHipsterApplication.class, args);
+	}
+
+}
